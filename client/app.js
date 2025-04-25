09e10e9269b8564f9ab1d3dbddb234449ec9df33
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-04-25 16:34:55 +0530

    completed

--- client/app.js
+++ client/app.js
@@ -1,22 +1,40 @@
-const socket = new WebSocket("ws://localhost:8080");
+//client side
+const socket = io("ws://localhost:3004");
 
 const form = document.querySelector("form");
+const input = document.querySelector("input");
+const p = document.querySelector("p");
+const ul = document.querySelector("ul");
 
 function sendMessage(e) {
   e.preventDefault();
-  const input = document.querySelector("input");
+
   if (input.value) {
-    socket.send(input.value);
+    socket.emit("message", input.value);
   }
-  input.value = '';
+  input.value = "";
 }
 
 form.addEventListener("submit", sendMessage);
 
-socket.addEventListener("message", ({ data }) => {
+// Receive message
+socket.on("message", (data) => {
   const li = document.createElement("li");
-  const ul = document.querySelector("ul");
-
   li.innerText = data;
   ul.append(li);
+  p.textContent = "";
+});
+
+// on typing
+
+ 
+
+let activityTimer;
+socket.on("activity", (userId) => {
+  p.textContent = `${userId} is typing...`;
+
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    p.textContent = "";
+  }, 2000);
 });
--- server/app.js
+++ server/app.js
@@ -1,19 +1,44 @@
-const { Server } = require("socket.io");
-const http = require("http");
+//server side
+import { Server } from "socket.io";
+import express from "express";
 
-const httpServer = http.createServer();
-httpServer.listen(8080);
+const PORT = process.env.PORT || 3004;
 
-const io = new Server(httpServer,{
-    cors:{
-        origin:'*'
-    }
+const app = express();
+
+const httpServer = app.listen(PORT, () => {
+  console.log("server connected");
+});
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log(`User ${socket.id} connected`);
-  io.on("message", (data) => {
+  const user = socket.id.substring(0, 5);
+
+  console.log(`User ${user} connected`);
+
+  // Upon connection - to the user
+  socket.emit("message", "Welcome to chat app");
+
+  // Upon connection - to other user
+  socket.broadcast.emit("message", `${user} joined`);
+
+  //on typing
+  socket.on("activity", () => {
+    socket.broadcast.emit("activity", `${user}`);
+  });
+ 
+  //send message
+  socket.on("message", (data) => {
     console.log("The message is ", data);
-    io.emit("message", `${socket.id.substring(0, 5)} : ${data}`);
+    io.emit("message", `${user} : ${data}`);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit('message',`${user} left `)
   });
 });
